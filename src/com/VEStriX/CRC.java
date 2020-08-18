0d70257c85407caa0a8b1611bfc7e5ea85657d57
Valentin Eremizin <eremizin@gmail.com>
2020-08-18 13:18:16 +0300

    Initial commit

---
+++ src/com/VEStriX/CRC.java
@@ -0,0 +1,265 @@
+package com.VEStriX;
+
+public class CRC {
+    private final String    CRC_name;           // name of CRC function
+    private final int       CRC_Width;          // Width of CRC: 8, 16, 32 or 64 bit
+    private final long      CRC_Poly;           // CRC polynomial
+    private final long      CRC_Poly_Reverse;   // CRC polynomial reverse value
+    private final long      CRC_Init;           // Init CRC value
+    private final boolean   CRC_RefIn;          // CRC Reflected In flag
+    private final boolean   CRC_RefOut;         // CRC Reflected Out flag
+    private final long      CRC_XorOut;         // CRC Xor out value
+    //private long    CRC_Check;          // CRC check value
+
+    public long[]  CRC8_Table       = new long[256];  // table for count the CRC-8
+    public long[]  CRC8_R_Table     = new long[256];  // table for count the CRC-8
+    public long[]  CRC16_Table      = new long[256]; // table for count the CRC-16
+    public long[]  CRC16_R_Table    = new long[256]; // table for count the CRC-16
+    public long[]  CRC32_Table      = new long[256];  // table for count the CRC-32
+    public long[]  CRC32_R_Table    = new long[256];  // table for count the CRC-32
+    public long[]  CRC64Table       = new long[256];  // table for count the CRC-64
+    public long[]  CRCTable         = new long[256];  // table for count the CRC
+
+    public CRC(String crc_name, int crc_width, long crc_poly, long crc_init, boolean crc_refin, boolean crc_refout, long crc_xorout)
+    {
+        this.CRC_name = crc_name;
+
+        if (crc_width != 8 && crc_width !=16 && crc_width != 32 && crc_width != 64) throw new IllegalArgumentException("Object \"" + CRC_name + "\" : CRC_width value " + crc_width + " is out of list: 8,16,32,64"); //check this for correct work!!!!
+        else this.CRC_Width = crc_width;
+
+        this.CRC_Poly = crc_poly;
+        this.CRC_Init = crc_init;
+        this.CRC_RefIn = crc_refin;
+        this.CRC_RefOut = crc_refout;
+        this.CRC_XorOut = crc_xorout;
+
+        this.CRC_Poly_Reverse = Reverse(this.CRC_Poly, this.CRC_Width);
+
+        if (this.CRC_Width == 8)
+        {
+            //Init_CRC_Table();  // this table for count the CRC checksumm
+            Init_CRC8_Table(); // this table for show for user;
+        }
+        else if (this.CRC_Width == 16)
+        {
+            //To DO
+            //Init_CRC_Table();  // this table for count the CRC checksumm
+            Init_CRC16_Table(); // this table for show for user;
+        }
+        else if (this.CRC_Width == 32)
+        {
+            //To DO
+            //Init_CRC_Table();  // this table for count the CRC checksumm
+            Init_CRC32_Table(); // this table for show for user;
+        }
+        else if (this.CRC_Width == 64)
+        {
+            //To DO
+            //Init_CRC_Table();  // this table for count the CRC checksumm
+            //Init_CRC32_Table(); // this table for show for user;
+        }
+    }
+
+    public String getCRC_name() {
+        return this.CRC_name;
+    }
+
+    public int getCRC_Width() {
+        return this.CRC_Width;
+    }
+
+    public long getCRC_Poly() {
+        return this.CRC_Poly;
+    }
+
+    public long getCRC_Poly_Reverse() {
+        return this.CRC_Poly_Reverse;
+    }
+
+    public long getCRC_Init() {
+        return this.CRC_Init;
+    }
+
+    public boolean getCRC_RefIn() {
+        return this.CRC_RefIn;
+    }
+
+    public boolean getCRC_RefOut() {
+        return this.CRC_RefOut;
+    }
+
+    public long getCRC_XorOut() {
+        return this.CRC_XorOut;
+    }
+
+    public static long Reverse(long data, int crcwidth)
+    {
+        long revers_data = 0;
+
+        byte[] array = new byte[crcwidth];
+        for(byte i = 0; i < crcwidth; i ++)
+        {
+            int power2 =  1 << i;
+            if ((data & power2) == power2) array[i] = 1;
+            else array[i] = 0;
+        }
+
+        for (int i = crcwidth-1; i >= 0; i--)
+        {
+            revers_data = revers_data | (array[i] << (crcwidth - 1 - i));
+        }
+        return revers_data;
+    }
+
+    public void Init_CRC8_Table()
+    {
+        long Value = 0;
+        long Mask = ((1 << this.CRC_Width) - 1);
+        long TopBit = (1 << (this.CRC_Width - 1));
+        if(!this.CRC_RefIn) {
+            for (long i = 0; i < 256; i++) {
+                Value = i << (this.CRC_Width - 8);
+
+                for (int j = 0; j < 8; j++) {
+                    if ((Value & TopBit) == TopBit) {
+                        Value = (Value << 1) ^ this.CRC_Poly;
+                    } else
+                        Value <<= 1;
+
+                    Value &= Mask;
+
+                }
+                this.CRC8_Table[(int) i] = Value;
+            }
+        }
+        if(this.CRC_RefIn) {
+            for (long i = 0; i < 256; i++) {
+
+                Value = i;
+
+                for (int j = 8; j > 0; j--) {
+                    if ((Value & 1) == 1) {
+                        Value = (Value >> 1) ^ this.CRC_Poly_Reverse;
+                    } else
+                        Value >>= 1;
+                }
+                this.CRC8_R_Table[(int)Reverse(i,8)] = Reverse(Value, this.CRC_Width);
+            }
+        }
+
+    }
+
+    public long CRC8Count(int[] buff, int bufflen)
+    {
+        long data;
+        long crc = this.CRC_Init;
+        for (int i = 0; i < bufflen; i++) {
+            if(!this.CRC_RefIn) crc = CRC8_Table[(int) (crc ^ buff[i])];
+            if(this.CRC_RefIn){
+                data = Reverse(buff[i], 8) ^ crc;
+                crc = CRC8_R_Table[(int)data];
+            }
+        }
+        crc = crc ^ this.CRC_XorOut;
+        if(this.CRC_RefOut)
+        {
+            crc = Reverse(crc, this.CRC_Width);
+        }
+        return crc; // return the crc
+    }
+
+    public void Init_CRC16_Table()
+    {
+        long Value = 0;
+        long Mask = ((1 << this.CRC_Width) - 1);
+        long TopBit = (1 << (this.CRC_Width - 1));
+        if(!this.CRC_RefIn) {
+            for (long i = 0; i < 256; i++) {
+
+                Value = i << (this.CRC_Width - 8);
+
+                for (int j = 0; j < 8; j++) {
+                    if ((Value & TopBit) == TopBit) {
+                        Value = (Value << 1) ^ this.CRC_Poly;
+                    } else
+                        Value <<= 1;
+
+                    Value &= Mask;
+
+                }
+                this.CRC16_Table[(int) i] = Value;
+            }
+        }
+        if(this.CRC_RefIn) {
+            for (long i = 0; i < 256; i++) {
+
+                Value = i;
+
+                for (int j = 8; j > 0; j--) {
+                    if ((Value & 1) == 1) {
+                        Value = (Value >> 1) ^ this.CRC_Poly_Reverse;
+                    } else
+                        Value >>= 1;
+                }
+                this.CRC16_R_Table[(int)Reverse(i,8)] = Reverse(Value, this.CRC_Width);
+            }
+        }
+    }
+
+    public long CRC16Count(int[] buff, int bufflen)
+    {
+        long data;
+        long crc = this.CRC_Init;
+        for (int i = 0; i < bufflen; i++) {
+            if(!this.CRC_RefIn) crc = (crc<<8) ^ CRC16_Table[(int) ((crc>>8) & 0xFF ^ buff[i])];
+            if(this.CRC_RefIn){
+                data = Reverse(buff[i], 8)^(crc>>8);
+                crc = CRC16_R_Table[(int)data] ^ (int)(0xFFFF & (crc << 8));
+            }
+        }
+        crc = crc ^ this.CRC_XorOut;
+        if(this.CRC_RefOut)
+        {
+            crc = Reverse(crc, this.CRC_Width);
+        }
+        return crc; // return the crc
+    }
+
+    public void Init_CRC32_Table()
+    {
+        long Value = 0;
+        long Mask = ((1 << this.CRC_Width) - 1);
+        long TopBit = (1 << (this.CRC_Width - 1));
+        if(!this.CRC_RefIn) {
+            for (long i = 0; i < 256; i++) {
+
+                Value = i << (this.CRC_Width - 8);
+
+                for (int j = 0; j < 8; j++) {
+                    if ((Value & TopBit) == TopBit) {
+                        Value = (Value << 1) ^ this.CRC_Poly;
+                    } else
+                        Value <<= 1;
+
+                    Value &= Mask;
+
+                }
+                this.CRC32_Table[(int) i] = Value;
+            }
+        }
+        if(this.CRC_RefIn) {
+            for (long i = 0; i < 256; i++) {
+
+                Value = i;
+
+                for (int j = 8; j > 0; j--) {
+                    if ((Value & 1) == 1) {
+                        Value = (Value >> 1) ^ this.CRC_Poly_Reverse;
+                    } else
+                        Value >>= 1;
+                }
+                this.CRC32_R_Table[(int)Reverse(i,8)] = Reverse(Value, this.CRC_Width);
+            }
+        }
+    }
+}
---
+++ src/com/VEStriX/JCRC.java
@@ -0,0 +1,140 @@
+package com.VEStriX;
+
+
+
+public class JCRC {
+
+        /*public static CRC CRC8 = new CRC("CRC-8", 8, 0x07, 0x00, false, false, 0x00);
+        public static CRC CRC8_CDMA2000 = new CRC("CRC-8/CDMA2000", 8, 0x9B, 0xFF, false, false, 0x00);
+        public static CRC CRC8_DARC = new CRC("CRC-8/DARC", 8, 0x39, 0x00, true, true, 0x00);
+        public static CRC CRC8_DVBS2 = new CRC("CRC-8/DVB-S2", 8, 0xD5, 0x00, false, false, 0x00);
+        public static CRC CRC8_EBU = new CRC("CRC-8/EBU", 8, 0x1D, 0xFF, true, true, 0x00);
+        public static CRC CRC8_ICODE = new CRC("CRC-8/ICODE", 8, 0x1D, 0xFD, false, false, 0x00);
+        public static CRC CRC8_ITU = new CRC("CRC-8/ITU", 8, 0x07, 0x00, false, false, 0x55);
+        public static CRC CRC8_MAXIM = new CRC("CRC-8/MAXIM", 8, 0x31, 0x00, true, true, 0x00);
+        public static CRC CRC8_ROHC = new CRC("CRC-8/ROHC", 8, 0x07, 0xFF, true, true, 0x00);
+        public static CRC CRC8_WCDMA = new CRC("CRC-8/WCDMA", 8, 0x9B, 0x00, true, true, 0x00);
+
+        public static CRC[] CRC8_All = {CRC8, CRC8_CDMA2000, CRC8_DARC, CRC8_DVBS2, CRC8_EBU, CRC8_ICODE, CRC8_ITU, CRC8_MAXIM, CRC8_ROHC, CRC8_WCDMA};
+
+        public static CRC CRC16_CCITT_FALSE = new CRC("CRC-16/CCITT-FALSE", 16, 0x1021, 0xFFFF, false, false, 0x0000);
+        public static CRC CRC16_ARC = new CRC("CRC-16/ARC", 16, 0x8005, 0x0000, true, true, 0x0000);
+        public static CRC CRC16_AUG_CCITT = new CRC("CRC-16/AUG-CCITT", 16, 0x1021, 0x1D0F, false, false, 0x0000);
+        public static CRC CRC16_BUYPASS = new CRC("CRC-16/BUYPASS", 16, 0x8005, 0x0000, false, false, 0x0000);
+        public static CRC CRC16_CDMA2000 = new CRC("CRC-16/CDMA2000", 16, 0xC867, 0xFFFF, false, false, 0x0000);
+        public static CRC CRC16_DDS_110 = new CRC("CRC-16/DDS-110", 16, 0x8005, 0x800D, false, false, 0x0000);
+        public static CRC CRC16_DECT_R = new CRC("CRC-16/DECT-R", 16, 0x0589, 0x0000, false, false, 0x0001);
+        public static CRC CRC16_DECT_X = new CRC("CRC-16/DECT-X", 16, 0x0589, 0x0000, false, false, 0x0000);
+        public static CRC CRC16_DNP = new CRC("CRC-16/DNP", 16, 0x3D65, 0x0000, true, true, 0xFFFF);
+        public static CRC CRC16_EN_13757 = new CRC("CRC-16/EN-13757", 16, 0x3D65, 0x0000, false, false, 0xFFFF);
+        public static CRC CRC16_GENIBUS = new CRC("CRC-16/GENIBUS", 16, 0x1021, 0xFFFF, false, false, 0xFFFF);
+        public static CRC CRC16_MAXIM = new CRC("CRC-16/MAXIM", 16, 0x8005, 0x0000, true, true, 0xFFFF);
+        public static CRC CRC16_MCRF4XX = new CRC("CRC-16/MCRF4XX", 16, 0x1021, 0xFFFF, true, true, 0x0000);
+        public static CRC CRC16_RIELLO = new CRC("CRC-16/RIELLO", 16, 0x1021, 0xB2AA, true, true, 0x0000);
+        public static CRC CRC16_T10_DIF = new CRC("CRC-16/T10-DIF", 16, 0x8BB7, 0x0000, false, false, 0x0000);
+        public static CRC CRC16_TELEDISK = new CRC("CRC-16/TELEDISK", 16, 0xA097, 0x0000, false, false, 0x0000);
+        public static CRC CRC16_TMS37157 = new CRC("CRC-16/TMS37157", 16, 0x1021, 0x89EC, true, true, 0x0000);
+        public static CRC CRC16_USB = new CRC("CRC-16/USB", 16, 0x8005, 0xFFFF, true, true, 0xFFFF);
+        public static CRC CRC16_A = new CRC("CRC-A", 16, 0x1021, 0xC6C6, true, true, 0x0000);
+        public static CRC CRC16_KERMIT = new CRC("CRC-16/KERMIT", 16, 0x1021, 0x0000, true, true, 0x0000);
+        public static CRC CRC16_MODBUS = new CRC("CRC-16/MODBUS", 16, 0x8005, 0xFFFF, true, true, 0x0000);
+        public static CRC CRC16_X25 = new CRC("CRC-16/X-25", 16, 0x1021, 0xFFFF, true, true, 0xFFFF);
+        public static CRC CRC16_XMODEM = new CRC("CRC-16/XMODEM", 16, 0x1021, 0x0000, false, false, 0x0000);
+
+        public static CRC[] CRC16_All = {CRC16_CCITT_FALSE, CRC16_ARC, CRC16_AUG_CCITT, CRC16_BUYPASS, CRC16_CDMA2000, CRC16_DDS_110, CRC16_DECT_R,
+                CRC16_DECT_X, CRC16_DNP, CRC16_EN_13757, CRC16_GENIBUS, CRC16_MAXIM, CRC16_MCRF4XX, CRC16_RIELLO, CRC16_T10_DIF,
+                CRC16_TELEDISK, CRC16_TMS37157, CRC16_USB, CRC16_A, CRC16_KERMIT, CRC16_MODBUS, CRC16_X25, CRC16_XMODEM};*/
+
+    public static void main(String[] args) {
+
+        try
+        {
+            // CRC CRC_with_error = new CRC("CRC-with-error", 9, 0x07, 0x00, false, false, 0x00); // This is test string for try/catch
+
+            CRC CRC8 = new CRC("CRC-8", 8, 0x07, 0x00, false, false, 0x00);
+            CRC CRC8_CDMA2000 = new CRC("CRC-8/CDMA2000", 8, 0x9B, 0xFF, false, false, 0x00);
+            CRC CRC8_DARC = new CRC("CRC-8/DARC", 8, 0x39, 0x00, true, true, 0x00);
+            CRC CRC8_DVBS2 = new CRC("CRC-8/DVB-S2", 8, 0xD5, 0x00, false, false, 0x00);
+            CRC CRC8_EBU = new CRC("CRC-8/EBU", 8, 0x1D, 0xFF, true, true, 0x00);
+            CRC CRC8_ICODE = new CRC("CRC-8/ICODE", 8, 0x1D, 0xFD, false, false, 0x00);
+            CRC CRC8_ITU = new CRC("CRC-8/ITU", 8, 0x07, 0x00, false, false, 0x55);
+            CRC CRC8_MAXIM = new CRC("CRC-8/MAXIM", 8, 0x31, 0x00, true, true, 0x00);
+            CRC CRC8_ROHC = new CRC("CRC-8/ROHC", 8, 0x07, 0xFF, true, true, 0x00);
+            CRC CRC8_WCDMA = new CRC("CRC-8/WCDMA", 8, 0x9B, 0x00, true, true, 0x00);
+
+            CRC[] CRC8_All = {CRC8, CRC8_CDMA2000, CRC8_DARC, CRC8_DVBS2, CRC8_EBU, CRC8_ICODE, CRC8_ITU, CRC8_MAXIM, CRC8_ROHC, CRC8_WCDMA};
+
+            CRC CRC16_CCITT_FALSE = new CRC("CRC-16/CCITT-FALSE", 16, 0x1021, 0xFFFF, false, false, 0x0000);
+            CRC CRC16_ARC = new CRC("CRC-16/ARC", 16, 0x8005, 0x0000, true, true, 0x0000);
+            CRC CRC16_AUG_CCITT = new CRC("CRC-16/AUG-CCITT", 16, 0x1021, 0x1D0F, false, false, 0x0000);
+            CRC CRC16_BUYPASS = new CRC("CRC-16/BUYPASS", 16, 0x8005, 0x0000, false, false, 0x0000);
+            CRC CRC16_CDMA2000 = new CRC("CRC-16/CDMA2000", 16, 0xC867, 0xFFFF, false, false, 0x0000);
+            CRC CRC16_DDS_110 = new CRC("CRC-16/DDS-110", 16, 0x8005, 0x800D, false, false, 0x0000);
+            CRC CRC16_DECT_R = new CRC("CRC-16/DECT-R", 16, 0x0589, 0x0000, false, false, 0x0001);
+            CRC CRC16_DECT_X = new CRC("CRC-16/DECT-X", 16, 0x0589, 0x0000, false, false, 0x0000);
+            CRC CRC16_DNP = new CRC("CRC-16/DNP", 16, 0x3D65, 0x0000, true, true, 0xFFFF);
+            CRC CRC16_EN_13757 = new CRC("CRC-16/EN-13757", 16, 0x3D65, 0x0000, false, false, 0xFFFF);
+            CRC CRC16_GENIBUS = new CRC("CRC-16/GENIBUS", 16, 0x1021, 0xFFFF, false, false, 0xFFFF);
+            CRC CRC16_MAXIM = new CRC("CRC-16/MAXIM", 16, 0x8005, 0x0000, true, true, 0xFFFF);
+            CRC CRC16_MCRF4XX = new CRC("CRC-16/MCRF4XX", 16, 0x1021, 0xFFFF, true, true, 0x0000);
+            CRC CRC16_RIELLO = new CRC("CRC-16/RIELLO", 16, 0x1021, 0xB2AA, true, true, 0x0000);
+            CRC CRC16_T10_DIF = new CRC("CRC-16/T10-DIF", 16, 0x8BB7, 0x0000, false, false, 0x0000);
+            CRC CRC16_TELEDISK = new CRC("CRC-16/TELEDISK", 16, 0xA097, 0x0000, false, false, 0x0000);
+            CRC CRC16_TMS37157 = new CRC("CRC-16/TMS37157", 16, 0x1021, 0x89EC, true, true, 0x0000);
+            CRC CRC16_USB = new CRC("CRC-16/USB", 16, 0x8005, 0xFFFF, true, true, 0xFFFF);
+            CRC CRC16_A = new CRC("CRC-A", 16, 0x1021, 0xC6C6, true, true, 0x0000);
+            CRC CRC16_KERMIT = new CRC("CRC-16/KERMIT", 16, 0x1021, 0x0000, true, true, 0x0000);
+            CRC CRC16_MODBUS = new CRC("CRC-16/MODBUS", 16, 0x8005, 0xFFFF, true, true, 0x0000);
+            CRC CRC16_X25 = new CRC("CRC-16/X-25", 16, 0x1021, 0xFFFF, true, true, 0xFFFF);
+            CRC CRC16_XMODEM = new CRC("CRC-16/XMODEM", 16, 0x1021, 0x0000, false, false, 0x0000);
+
+            CRC[] CRC16_All = {CRC16_CCITT_FALSE, CRC16_ARC, CRC16_AUG_CCITT, CRC16_BUYPASS, CRC16_CDMA2000, CRC16_DDS_110, CRC16_DECT_R,
+                    CRC16_DECT_X, CRC16_DNP, CRC16_EN_13757, CRC16_GENIBUS, CRC16_MAXIM, CRC16_MCRF4XX, CRC16_RIELLO, CRC16_T10_DIF,
+                    CRC16_TELEDISK, CRC16_TMS37157, CRC16_USB, CRC16_A, CRC16_KERMIT, CRC16_MODBUS, CRC16_X25, CRC16_XMODEM};
+
+            CRC CRC_32          = new CRC("CRC-32",         32, 0x04C11DB7, 0xFFFFFFFF, true, true, 0xFFFFFFFF);
+            CRC CRC_32_BZIP2    = new CRC("CRC-32/BZIP2",   32, 0x04C11DB7, 0xFFFFFFFF, false, false, 0xFFFFFFFF);
+
+            CRC[] CRC32_All = {CRC_32, CRC_32_BZIP2};
+
+//            for(int i = 0; i< CRC16_All.length; i++)
+//            {
+//                System.out.println(CRC16_All[i].getCRC_name());
+//            }
+            System.out.println();
+
+            for(int i = 1; i<=256; i++)
+            {
+                if((i % 16 == 0 && i != 0) || i==16) System.out.println("0x" + Long.toHexString(CRC16_RIELLO.CRC16_R_Table[i-1]) + " ");
+                else System.out.print("0x" + Long.toHexString(CRC16_RIELLO.CRC16_R_Table[i-1]) + " ");
+            }
+            System.out.println();
+
+            for(int i = 1; i<=256; i++)
+            {
+                if((i % 16 == 0 && i != 0) || i==16) System.out.println("0x" + Long.toHexString(CRC_32.CRC32_R_Table[i-1]) + " ");
+                else System.out.print("0x" + Long.toHexString(CRC_32.CRC32_R_Table[i-1]) + " ");
+            }
+
+            int[] buff = {0x12, 0x34, 0x56, 0x78, 0x09};
+
+            System.out.println();
+            System.out.println("Результат расчёта CRC " + CRC16_RIELLO.getCRC_name() + " значениея 0x12 0x34 0x56 0x78 0x09 равен 0х" + Long.toHexString(CRC16_RIELLO.CRC16Count(buff, buff.length)));
+            System.out.println();
+
+            for(int i = 0; i < CRC8_All.length; i++){
+                System.out.println();
+                System.out.println("Результат расчёта CRC " + CRC8_All[i].getCRC_name() + " значениея 0x12 0x34 0x56 0x78 0x09 равен 0х" + Long.toHexString(CRC8_All[i].CRC8Count(buff, buff.length)));
+            }
+        }
+        catch(IllegalArgumentException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println("We got " + e.toString());
+        }
+
+    }
+}
